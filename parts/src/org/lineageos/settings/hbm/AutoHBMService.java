theangelofdead01/void_device_xiaomi_sm8250-common

sm8250-common: parts: Implement HBM & Auto HBM
Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

parts/src/org/lineageos/settings/hbm/AutoHBMService.java
@@ -1,0 +1,120 @@
+package org.lineageos.settings.hbm;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
+
+public class AutoHBMService extends Service {
+    private static final String HBM_FILE = "/sys/class/drm/card0/card0-DSI-1/disp_param";
+
+    private static boolean mAutoHBMActive = false;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
+
+    public void activateLightSensorRead() {
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void deactivateLightSensorRead() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        mAutoHBMActive = false;
+        enableHBM(false);
+    }
+
+    private void enableHBM(boolean enable) {
+        if (enable) {
+            FileUtils.writeValue(HBM_FILE, "0x10000");
+        } else {
+            FileUtils.writeValue(HBM_FILE, "0xF0000");
+        }
+    }
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            KeyguardManager km =
+                    (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
+            boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
+            float threshold = Float.parseFloat(mSharedPrefs.getString(HBMFragment.KEY_AUTO_HBM_THRESHOLD, "20000"));
+            if (lux > threshold) {
+                if ((!mAutoHBMActive | !keyguardShowing)) {
+                    mAutoHBMActive = true;
+                    enableHBM(true);
+                }
+            }
+            if (lux < threshold) {
+                if (mAutoHBMActive) {
+                    mAutoHBMActive = false;
+                    enableHBM(false);
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

parts/src/org/lineageos/settings/hbm/HBMModeSwitch.java
@@ -1,0 +1,49 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.settings.hbm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
+
+public class HBMModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/class/drm/card0/card0-DSI-1/disp_param";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "0x10000" : "0xF0000");
+        return true;
+    }
+}

AnkitSavani111/old_android_device_xiaomi_sm8250-common

parts/src/org/lineageos/settings/hbm/AutoHBMService.java
@@ -1,0 +1,120 @@
+package org.lineageos.settings.hbm;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
+
+public class AutoHBMService extends Service {
+    private static final String HBM = "/sys/class/drm/card0/card0-DSI-1/disp_param";
+
+    private static boolean mAutoHBMActive = false;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
+
+    public void activateLightSensorRead() {
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void deactivateLightSensorRead() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        mAutoHBMActive = false;
+        enableHBM(false);
+    }
+
+    private void enableHBM(boolean enable) {
+        if (enable) {
+            FileUtils.writeLine(HBM, "0x10000");
+        } else {
+            FileUtils.writeLine(HBM, "0xF0000");
+        }
+    }
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            KeyguardManager km =
+                    (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
+            boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
+            float threshold = Float.parseFloat(mSharedPrefs.getString(HBMFragment.KEY_AUTO_HBM_THRESHOLD, "20000"));
+            if (lux > threshold) {
+                if ((!mAutoHBMActive | !keyguardShowing)) {
+                    mAutoHBMActive = true;
+                    enableHBM(true);
+                }
+            }
+            if (lux < threshold) {
+                if (mAutoHBMActive) {
+                    mAutoHBMActive = false;
+                    enableHBM(false);
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

parts/src/org/lineageos/settings/hbm/HBMModeSwitch.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.settings.hbm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
+
+public class HBMModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String HBM = "/sys/class/drm/card0/card0-DSI-1/disp_param";
+
+    public static String getHBM() {
+        if (FileUtils.isFileWritable(HBM)) {
+            return HBM;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeLine(getHBM(), enabled ? "0x10000" : "0xF0000");
+        return true;
+    }
+}