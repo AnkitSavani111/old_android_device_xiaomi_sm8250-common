28e31632aca34c1c7c49db46b00fd82bb7643f10
Ankit Savani <ankitmsavani12345@gmail.com>
2023-11-24 09:02:20 +0000

    Revert "sm8250-common: Remove unnecessary resource"
    This reverts commit 1ccb35c9a76e6d7fcb053e05a4e1217ba344de5e.

--- kona.mk
+++ kona.mk
@@ -366,7 +366,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
